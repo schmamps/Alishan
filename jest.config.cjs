cb719fda5271e24eaa866e3c16319d48ecc5d7a1
Andrew Champion <awchampion@gmail.com>
2020-03-07 12:39:57 -0800

    retry for travis env

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,12 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	modulePathIgnorePatterns: ['src/.*', 'test/.+\.js']
+	modulePathIgnorePatterns: ['src/.*', 'test/.+\.js'],
+	globals: {
+		'ts-jest': {
+			diagnostics: {
+				warnOnly: true,
+			},
+		},
+	},
 };
