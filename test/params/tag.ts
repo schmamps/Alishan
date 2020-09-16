25aa118b3690e148b2338348a1ca36114910375a
Andrew Champion <awchampion@gmail.com>
2020-09-16 16:10:54 -0700

    reimplement Array.flat(Infinity) for TS

--- test/params/tag.ts
+++ test/params/tag.ts
@@ -1,7 +1,24 @@
-export default (...strs: (string | string[])[]) => {
-	const comps = strs.
-		// @ts-ignore
-		flat(Infinity).
+
+type ArrayOfStringUnionStringArray = (string | string[])
+
+
+/**
+ * Flatten array of strings & string array members
+ * (because `.flat(Infinity)` isn't passing)
+**/
+const flatten = (items: ArrayOfStringUnionStringArray[]): string[] => {
+	const flat: string[] = [];
+
+	for (const item of items) {
+		flat.concat(item)
+	}
+
+	return flat;
+};
+
+
+export default (...strs: ArrayOfStringUnionStringArray[]) => {
+	const comps = flatten(strs).
 		map((comp) => comp.toLowerCase()).
 		map((comp) => comp.replace(/[\-\s]+/g, '-')).
 		map((comp) => comp.replace(/^\-*(.*?)\-*$/, '$1')).
